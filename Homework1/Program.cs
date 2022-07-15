d733e17f97a9e0bc3f7e33f06911936a16a6ebd9
Ivan <vanesiotanaev@mail.ru>
2022-07-15 13:36:28 +0300

    HW2 completed

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+/*
 int num1, num2;
 
 Console.Write("Please, enter the first number :");
@@ -71,3 +71,86 @@ else
     current = current + 2;
     }
 }
+
+
+// Домашнее задание №2. 15.07.2022
+
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondNumMethod (int number)
+{
+    int num2 = (number / 10) % 10;
+    return num2;
+}
+
+Console.Write("Please, enter the three-digit number: ");
+int usernum = Convert.ToInt32(Console.ReadLine());
+
+while (usernum < 100 || usernum > 999)
+{
+    Console.Write("The number is not three-digit! Please, enter the three-digit number: ");
+    usernum = Convert.ToInt32(Console.ReadLine());
+}
+    int answer = SecondNumMethod(usernum);
+    Console.Write("The second digit of this number is " + answer);
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ThirdDigitMethod (int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("There is no 3rd digit in number " + number);
+        return 0;
+    }
+    else
+    {
+        Console.WriteLine("The number has 3 (or more) digits in it");
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        number = number % 10;
+        Console.Write ("The third digit is " + number);
+        return number;
+    }
+}
+
+Console.Write("Please, enter the number: ");
+int usernum = Convert.ToInt32(Console.ReadLine());
+int answer = ThirdDigitMethod (usernum);
+
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool WeekDayMethod (int daynumb)
+{
+    while (daynumb > 7 || daynumb < 1)
+    {
+        Console.Write("There is no such day of the week. Please, enter the correct number of the day: ");
+        daynumb = Convert.ToInt32(Console.ReadLine());
+    }
+    if (daynumb <= 5)
+    {
+    if (daynumb == 1) Console.Write("Monday is not a weekend!"); 
+    if (daynumb == 2) Console.Write("Tuesday is not a weekend!");
+    if (daynumb == 3) Console.Write("Wednesday is not a weekend!");
+    if (daynumb == 4) Console.Write("Thursday is not a weekend!");
+    if (daynumb == 5) Console.Write("Friday is almost the weekend, but it is not Saturday or Sunday!");
+
+    return false;
+    }
+    
+    else
+    {
+    if (daynumb == 6) Console.Write("Saturday is the weekend! Cool!");
+    if (daynumb == 7) Console.Write("Sunday is the weekend! Cool!");
+    
+    return true;
+    }
+}
+
+Console.Write("Please, enter the number: ");
+int userdaynum = Convert.ToInt32(Console.ReadLine());
+bool answer = WeekDayMethod (userdaynum);
